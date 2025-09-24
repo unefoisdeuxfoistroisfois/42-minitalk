d7d61a129150cdfc87930988e9d3fb4b30956435
unefoisdeuxfoistroisfois <1fois2fois3fois@gmail.com>
2025-09-24 21:56:30 +0200

    Début du ack

--- server.c
+++ server.c
@@ -6,25 +6,22 @@
 /*   By: britela- <britela-@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/14 19:09:08 by britela-          #+#    #+#             */
-/*   Updated: 2025/09/22 16:46:11 by britela-         ###   ########.fr       */
+/*   Updated: 2025/09/24 16:58:26 by britela-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void	ft_receptionBit(int sigusr)
+void	ft_receptionBit(int sigusr, siginfo_t *info, void *content)
 {
+		void(context);
 	static	int	tabBit[8];
 	static	int	i;
 
 	if (sigusr == SIGUSR1)
-	{
 		tabBit[i] = 1;
-	}
 	else if (sigusr == SIGUSR2)
-	{
 		tabBit[i] = 0;
-	}
 	i++;
 	if (i == 8)
 	{
@@ -40,17 +37,21 @@ void	ft_receptionBit(int sigusr)
 		}
             	write(1, &c, 1);
 		i = 0;
+	kill(info->si_pid, SIGUSR1);
 	}
 }
 
 void	ft_discussion(void)
 {
-	int nombreInfini;
+	int	nombreInfini;
+	siginfo_t	info= ft_receptionBit();
 
 	nombreInfini = 1;
-	signal(SIGUSR1, ft_receptionBit);
+	sigaction(SIGUSR1, info, NULL);
+//	signal(SIGUSR1, ft_receptionBit);
 	write(1, "\n", 1);
-	signal(SIGUSR2, ft_receptionBit);
+	sigaction(SIGUSR2, info, NULL);
+//	signal(SIGUSR2, ft_receptionBit);
 
 	while(nombreInfini)
 	{
