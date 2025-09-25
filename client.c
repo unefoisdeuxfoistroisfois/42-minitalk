4417883b11d16468b29c0984b141b1da6dc0997c
unefoisdeuxfoistroisfois <1fois2fois3fois@gmail.com>
2025-09-25 16:44:25 +0200

    Début de norminette pour client.c et serveur.c

--- client.c
+++ client.c
@@ -6,70 +6,68 @@
 /*   By: britela- <britela-@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/14 22:33:26 by britela-          #+#    #+#             */
-/*   Updated: 2025/09/25 15:24:18 by britela-         ###   ########.fr       */
+/*   Updated: 2025/09/25 16:29:17 by britela-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-static volatile sig_atomic_t get_ack = 0; // au lieu de int
+static volatile sig_atomic_t	g_ack = 0;
 
 void	reception_ack(int sig)
 {
 	(void)sig;
-	get_ack = 1;
+	g_ack = 1;
 }
 
-void	ft_conversionEnBit(pid_t argsPid, char c)
+void	ft_convertinbit(pid_t argsPid, char c)
 {
 	int	i;
-	int	newBit[8];
+	int	newbit[8];
 
 	i = 0;
 	while (i < 8)
 	{
-		newBit[i] = c % 2;
+		newbit[i] = c % 2;
 		c = c / 2;
 		i++;
 	}
 	i = 7;
 	while (i >= 0)
 	{
-		if (newBit[i] == 1)
+		if (newbit[i] == 1)
 			kill(argsPid, SIGUSR1);
-		else if (newBit[i] == 0)
+		else if (newbit[i] == 0)
 			kill(argsPid, SIGUSR2);
-		while(get_ack != 1)
+		while (get_ack != 1)
 		{
 			pause();
 		}
-		get_ack = 0;
+		g_ack = 0;
 		i--;
 	}
 }
 
 int	main(int argc, char **argv)
 {
-	int 	nPid;
+	int		npid;
+	int		i;
 	char	*message;
-	int	i;
 
 	if (argc != 3)
 	{
 		ft_printf("ciao : ");
 		return (-1);
 	}
-	nPid = ft_atoi(argv[1]);
+	npid = ft_atoi(argv[1]);
 	message = argv[2];
-
 	signal(SIGUSR1, reception_ack);
-	
 	i = 0;
 	while (message[i] != '\0')
 	{
-		ft_conversionEnBit(nPid, message[i]);
+		ft_convertinbit(npid, message[i]);
 		i++;
 	}
-	ft_conversionEnBit(nPid, '\0');   // insère un saut de ligne immédiat
+	ft_convertinbit(npid, '\0');
 	return (0);
 }
--- server.c
+++ server.c
@@ -6,18 +6,20 @@
 /*   By: britela- <britela-@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/14 19:09:08 by britela-          #+#    #+#             */
-/*   Updated: 2025/09/25 15:28:37 by britela-         ###   ########.fr       */
+/*   Updated: 2025/09/25 16:16:41 by britela-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void	ft_receptionBit(int sigusr, siginfo_t *info, void *content)
+void	ft_receptbit(int sigusr, siginfo_t *info, void *content)
 {
-	(void)content;
-	static	int	tabBit[8];
-	static	int	i;
+	static int	tabbit[8];
+	static int	i;
+	int			j;
+	char		c;
 
+	(void)content;
 	if (sigusr == SIGUSR1)
 		tabBit[i] = 1;
 	else if (sigusr == SIGUSR2)
@@ -25,9 +27,6 @@ void	ft_receptionBit(int sigusr, siginfo_t *info, void *content)
 	i++;
 	if (i == 8)
 	{
-		int	j;
-		char	c;
-		
 		c = 0;
 		j = 0;
 		while (j < 8)
@@ -41,39 +40,33 @@ void	ft_receptionBit(int sigusr, siginfo_t *info, void *content)
 			write(1, &c, 1);
 		i = 0;
 	}
-	if (info && info->si_pid)
-		kill(info->si_pid, SIGUSR1); // ACK
+	kill(info->si_pid, SIGUSR2);
 }
 
 void	ft_discussion(void)
 {
-	int	nombreInfini;
+	struct sigaction	action;
+	int					ninfini;
 
-	nombreInfini = 1;
-	struct	sigaction action;
-    	sigemptyset(&action.sa_mask);
+	ninfini = 1;
+	sigemptyset(&action.sa_mask);
 	action.sa_sigaction = ft_receptionBit;
 	action.sa_flags = SA_SIGINFO;
-
 	sigaction(SIGUSR1, &action, NULL);
 	sigaction(SIGUSR2, &action, NULL);
-	write(1, "\n", 1);
-//	signal(SIGUSR1, ft_receptionBit);
-//	signal(SIGUSR2, ft_receptionBit);
-
-	while(nombreInfini)
+	write (1, "\n", 1);
+	while (ninfini)
 	{
 		pause();
 	}
 }
 
-int	main()
+int	main(void)
 {
-	int	varpid; //La fonction pid renvoie un entier
+	int	varpid;
 
-	varpid = getpid(); 
-	ft_printf("PID : (%d)", varpid);
-
+	varpid = getpid();
+	ft_printf ("PID : (%d)", varpid);
 	ft_discussion();
-	return(0);
+	return (0);
 }
